a9dedb24d7df93f763f86eec7dcd7997c49a8abc
alisaLachman <alisal@wix.com>
2023-04-09 16:35:32 +0300

    added padding

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,19 @@ function App() {
     return (
         <div className={styles.App}>
             <form className={styles.form}>
-                <div className={styles.input}>
+                <div className={styles.input_div}>
                     <label className={styles.name} id={'name'} name={'name'}>
                         name
                     </label>
-                    <input id={'name'} name={'name'} />
+                    <input id={'name'} name={'name'} className={styles.input} />
                 </div>
-                <div className={styles.input}>
+                <div className={styles.input_div}>
                     <label>Email</label>
-                    <input id={'email'} name={'email'} />
+                    <input id={'email'} name={'email'} className={styles.input} />
                 </div>
-                <div className={styles.input}>
+                <div className={styles.input_div}>
                     <label>Massage </label>
-                    <input id={'Massage '} name={'Massage '} />
+                    <input id={'Massage '} name={'Massage '} className={styles.input} />
                 </div>
                 <button className={styles.button}>Submit</button>
             </form>
