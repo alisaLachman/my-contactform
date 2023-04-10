714b86daff3aef2557f4db440951446609b10abd
alisaLachman <alisal@wix.com>
2023-04-10 10:24:11 +0300

    Submit color change, added poop up shadow+borders, contact us H1+paragraph

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
 import styles from './App.module.scss';
+import Classnames from 'classnames';
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <div className={styles.App}>
+            <div className={styles.contact_Us_Padding}>
+                <h2 className={styles.H1_Form}>Contact Us</h2>
+                <p className={styles.Paragraph_Form}>
+                    Have a question? Fill out the form bellow and we&apos;ll get to you as soon as
+                    we can
+                </p>
+                <p className={Classnames(styles.Paragraph_Form, styles.Grey_Paragraph)}>
+                    all fields are required
+                </p>
+            </div>
             <form className={styles.form}>
                 <div className={styles.input_div}>
                     <label className={styles.name} id={'name'} name={'name'}>
@@ -21,7 +32,9 @@ function App() {
                     <label>Massage </label>
                     <input id={'Massage '} name={'Massage '} className={styles.input} />
                 </div>
-                <button className={styles.button}>Submit</button>
+                <div className={styles.buttonPadding}>
+                    <button className={styles.button}>Submit</button>
+                </div>
             </form>
         </div>
     );
